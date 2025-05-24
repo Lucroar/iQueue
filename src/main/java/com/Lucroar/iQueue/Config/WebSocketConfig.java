c6aa787c9b433214146506fe98095fbaf8f45d5c
Lucroar <marwinbnimes@gmail.com>
2025-05-24 18:34:53 +0800

    edited allowed origins

--- src/main/java/com/Lucroar/iQueue/Config/WebSocketConfig.java
+++ src/main/java/com/Lucroar/iQueue/Config/WebSocketConfig.java
@@ -12,7 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/queue-websocket")
-                .setAllowedOrigins("*")
+                .setAllowedOriginPatterns("*")
                 .withSockJS()
                 .setSessionCookieNeeded(false);
     }
