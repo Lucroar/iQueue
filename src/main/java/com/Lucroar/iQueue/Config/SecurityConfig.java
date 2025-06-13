d7eb79d92794d3213df91f8209d78f1df23df90a
Lucroar <marwinbnimes@gmail.com>
2025-06-13 18:32:41 +0800

    updated endpoint permission

--- src/main/java/com/Lucroar/iQueue/Config/SecurityConfig.java
+++ src/main/java/com/Lucroar/iQueue/Config/SecurityConfig.java
@@ -57,9 +57,9 @@ public class SecurityConfig {
                                 .jwtAuthenticationConverter(jwtConverter)))
                 .authorizeHttpRequests(auth -> auth
 //                        .requestMatchers("/cashier/**").hasRole("CASHIER")
-                        .requestMatchers("/cashier/new-cashier", "/cashier/login", "/cashier/hello").permitAll()
-                        .requestMatchers( "/customer/register", "customer/login", "customer/send-otp",
-                                "customer/verify-otp").permitAll()
+                        .requestMatchers( "/staff/**").permitAll()
+                        .requestMatchers( "/customer/register", "/customer/login", "/customer/send-otp",
+                                "/customer/verify-otp").permitAll()
                         .requestMatchers("/queue-websocket/**").permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(session -> session
